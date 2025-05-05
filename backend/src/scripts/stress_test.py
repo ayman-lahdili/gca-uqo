6c4ed9d0982e88cc1d360eb1a730f8fb81d430dd
ayman-lahdili <aymanlahdili25@gmail.com>
2025-05-05 19:32:35 -0400

    Stress

--- backend/src/scripts/stress_test.py
+++ backend/src/scripts/stress_test.py
@@ -7,3 +7,8 @@ class WebsiteUser(HttpUser):
     @task
     def get_uqo_courses(self):
         self.client.get("/v1/uqo/cours?departement=DII")
+    
+    @task
+    def get_uqo_programmes(self):
+        for cycle in range(1, 4):
+            self.client.get(f"/v1/uqo/programmes?departement=INFOR&cycle={cycle}")
